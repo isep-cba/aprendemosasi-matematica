f9276a00dc2210bd4c025a458113f261928b5e74
moviedo <moviedo@isep-cba.edu.ar>
2024-10-03 09:23:32 -0300

    Agrega funcion modales de principios

--- funciones.js
+++ funciones.js
@@ -7,3 +7,15 @@ function openNav() {
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
   }
+
+
+  function openMod1() {
+    document.getElementById("mod1").style.height = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeMod1() {
+    document.getElementById("mod1").style.height = "0%";
+  }
+
+
