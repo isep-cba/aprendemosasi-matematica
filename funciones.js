ee08116fe764ce49458c0d4cea9e1fdce9f7f90b
moviedo <moviedo@isep-cba.edu.ar>
2024-10-24 12:57:01 -0300

    Agrega acercade y reemplaza audio

--- funciones.js
+++ funciones.js
@@ -19,3 +19,12 @@ function openNav() {
   }
 
 
+
+  function openAcercade() {
+    document.getElementById("acercade").style.height = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeAcercade() {
+    document.getElementById("acercade").style.height = "0%";
+  }
