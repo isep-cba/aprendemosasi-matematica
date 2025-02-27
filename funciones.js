e1b4f1bf3c803774adc9b023e8ca0d79748c3978
moviedo <moviedo@isep-cba.edu.ar>
2025-02-27 15:07:26 -0300

    Menu lateral angosto

--- funciones.js
+++ funciones.js
@@ -1,16 +1,12 @@
-
-
-/* Open when someone clicks on the span element */
+/* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-    document.getElementById("body").style.overflowY = "hidden";
-  }
-  
-  /* Close when someone clicks on the "x" symbol inside the overlay */
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-    document.getElementById("body").style.overflowY = "scroll";
-  }
+  document.getElementById("mySidenav").style.width = "350px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
 
 
   function openMod1() {
