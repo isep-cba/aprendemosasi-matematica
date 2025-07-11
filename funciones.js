9ccfe6ca195f328e871a6c431d7b45e888746022
moviedo <moviedo@isep-cba.edu.ar>
2025-07-11 15:43:35 -0300

    Sidenav desplaza al main, mensaje de scroll animado

--- funciones.js
+++ funciones.js
@@ -7,15 +7,18 @@ function videoPause() {
 }
 
 
-/* Set the width of the side navigation to 250px */
+/* Set the width of the side navigation to 350px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "350px";
+  document.getElementById("main").style.marginLeft = "350px";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.width = "0px";
+  document.getElementById("main").style.marginLeft = "0px";
 }
+
 
 
   function openMod1() {
